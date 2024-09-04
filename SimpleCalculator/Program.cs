d521b2cebf28d5427d88e12d56ecc13f4037f193
kylianLaw <iladelek@gmail.com>
2024-09-04 11:02:02 -0400

    first part of part 2 is done

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Data;
+using CalcLibrary;
 namespace SimpleCalculator
 {
     class Program
@@ -8,19 +9,49 @@ namespace SimpleCalculator
         {
             try
             {
-              //allooooooooooooooooooooo
-              //lol
+
                 // Class to perform actual calculations
                 CalculatorEngine calculatorEngine = new CalculatorEngine();
+                Boolean work = false;
+                double firstNumber= 0;
+                double secondNumber = 0;
+                while (work == false)
+                {
+                    Console.WriteLine("Enter a first value");
+                    firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine(), out work);
+                    switch (work) {
 
-                double firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
-                double secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+                        case true:
+                            break;
+                        case false:
+                            Console.WriteLine("the conversion as failed, enter a valid number");
+                            break;
+                    }
+                
+                }
+                work = false;
+                while (work == false)
+                {
+                    Console.WriteLine("Enter a second value");
+                     secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine(), out work);
+                    switch (work)
+                    {
+
+                        case true:
+                            break;
+                        case false:
+                            Console.WriteLine("the conversion as failed, enter a valid number");
+                            break;
+                    }
+                }
+
                 string operation = Console.ReadLine();
 
                 double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
-                Console.WriteLine(result);
-
+                
+                Console.WriteLine("The value {0} {1} the value {2} equals to " + String.Format("{3:.##}", firstNumber, operation, secondNumber, result));
+               
             } catch (Exception ex)
             {
                 // Normally, we'd log this error to a file.
