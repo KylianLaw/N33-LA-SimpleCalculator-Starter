6eae37946a8cdba5a427fcd69bfcec6007a6b2ae
kylianLaw <iladelek@gmail.com>
2024-09-10 11:29:08 -0400

    test case for input converter

--- SimpleCalculator.Test.Unit/InputConverterTest.cs
+++ SimpleCalculator.Test.Unit/InputConverterTest.cs
@@ -8,21 +8,65 @@ namespace SimpleCalculator.Test.Unit
     {
         private readonly InputConverter _inputConverter = new InputConverter();
 
+        //Success test of the valide string into double number
         [TestMethod]
         public void ConvertsValidStringInputIntoDouble()
         {
             string inputNumber = "5";
-            double convertedNumber = _inputConverter.ConvertInputToNumeric(inputNumber);
+            double convertedNumber = InputConverter.ConvertInputToNumeric(inputNumber, out bool success);
             Assert.AreEqual(5, convertedNumber);
         }
-
+        //Testing the fail condition of the conversion from string to double number
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void FailsToConvertsInvalidStringInputIntoDouble()
         {
             string inputNumber = "*";
-            double convertedNumber = _inputConverter.ConvertInputToNumeric(inputNumber);
-            Assert.AreEqual(2, convertedNumber);
+            double convertedNumber = InputConverter.ConvertInputToNumeric(inputNumber, out bool success);
+            Assert.AreEqual(5, convertedNumber);
+        }
+        //Success test of the addition operator
+       [TestMethod]
+       public void ConvertsValidStringInputOperationAdd()
+        {
+            string inputAddition = "+";
+            bool convertedAddition = InputConverter.ValidOperation(inputAddition);
+            Assert.AreEqual(true, convertedAddition);
+        }
+        //Success test of the soustraction operator
+        [TestMethod]
+        public void ConvertsValidStringInputOperationSoustraction()
+        {
+            string inputSoustraction = "-";
+            bool convertedSoustraction = InputConverter.ValidOperation(inputSoustraction);
+            Assert.AreEqual(true, convertedSoustraction);
+        }
+        //Success test of the multiplayer operator
+        [TestMethod]
+        public void ConvertsValidStringInputOperationMultipliyer()
+        {
+            string inputMultipliyer = "*";
+            bool convertedMultipliyer = InputConverter.ValidOperation(inputMultipliyer);
+            Assert.AreEqual(true, convertedMultipliyer);
+        }
+        //Success test of the Divide operator
+        [TestMethod]
+        public void ConvertsValidStringInputOperationDivide()
+        {
+            string inputDivide = "/";
+            bool convertedDivide = InputConverter.ValidOperation(inputDivide);
+            Assert.AreEqual(true, convertedDivide);
+        }
+
+        [TestMethod]
+        public void FailsConvertsValidStringInputOperation()
+        {
+            string inputOperation = "ark";
+            bool convertedOperation = InputConverter.ValidOperation(inputOperation);
+            Assert.AreEqual(true, convertedOperation);
+
         }
     }
+   
+
 }
